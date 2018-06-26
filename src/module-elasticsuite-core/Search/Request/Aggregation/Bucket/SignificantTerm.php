ee056b7285ad7da8a2ba574ecfcb65cbe2a45781
Aurélien FOUCRET <aurelien.foucret@smile.fr>
2018-06-26 13:41:39 +0200

    Support for significant search term aggs.

--- src/module-elasticsuite-core/Search/Request/Aggregation/Bucket/SignificantTerm.php
+++ src/module-elasticsuite-core/Search/Request/Aggregation/Bucket/SignificantTerm.php
@@ -18,23 +18,36 @@ use Smile\ElasticsuiteCore\Search\Request\BucketInterface;
 use Smile\ElasticsuiteCore\Search\Request\QueryInterface;
 
 /**
- * Term Bucket implementation.
+ * Significant term bucket implementation.
  *
  * @category Smile
  * @package  Smile\ElasticsuiteCore
  * @author   Aurelien FOUCRET <aurelien.foucret@smile.fr>
  */
-class Term extends AbstractBucket
+class SignificantTerm extends AbstractBucket
 {
+    const ALGORITHM_GND = 'gnd';
+
+    const ALGORITHM_CHI_SQUARE = 'chi_sqare';
+
+    const ALGORITHM_JLH = 'jlh';
+
+    const ALGORITHM_PERCENTAGE = 'percentage';
+
     /**
      * @var integer
      */
     private $size;
 
     /**
+     * @var integer
+     */
+    private $minDocCount;
+
+    /**
      * @var string
      */
-    private $sortOrder;
+    private $algorithm;
 
     /**
      * Constructor.
@@ -47,7 +60,7 @@ class Term extends AbstractBucket
      * @param QueryInterface    $filter       Bucket filter.
      * @param QueryInterface    $nestedFilter Nested filter for the bucket.
      * @param integer           $size         Bucket size.
-     * @param string            $sortOrder    Bucket sort order.
+     * @param string            $algotithm    Algorithm used
      */
     public function __construct(
         $name,
@@ -58,12 +71,14 @@ class Term extends AbstractBucket
         QueryInterface $filter = null,
         QueryInterface $nestedFilter = null,
         $size = 0,
-        $sortOrder = BucketInterface::SORT_ORDER_COUNT
+        $minDocCount = 5,
+        $algotithm = self::ALGORITHM_GND
     ) {
         parent::__construct($name, $field, $metrics, $childBuckets, $nestedPath, $filter, $nestedFilter);
 
-        $this->size      = $size > 0 && $size < self::MAX_BUCKET_SIZE ? $size : self::MAX_BUCKET_SIZE;
-        $this->sortOrder = $sortOrder;
+        $this->minDocCount = $minDocCount;
+        $this->algorithm   = $algotithm;
+        $this->size        = $size > 0 && $size < self::MAX_BUCKET_SIZE ? $size : self::MAX_BUCKET_SIZE;
     }
 
     /**
@@ -71,7 +86,7 @@ class Term extends AbstractBucket
      */
     public function getType()
     {
-        return BucketInterface::TYPE_TERM;
+        return BucketInterface::TYPE_SIGNIFICANT_TERM;
     }
 
     /**
@@ -84,13 +99,13 @@ class Term extends AbstractBucket
         return $this->size;
     }
 
-    /**
-     * Bucket sort order.
-     *
-     * @return string
-     */
-    public function getSortOrder()
+    public function getMinDocCount()
     {
-        return $this->sortOrder;
+        return $this->minDocCount;
+    }
+
+    public function getAlgorithm()
+    {
+        return $this->algorithm;
     }
 }
